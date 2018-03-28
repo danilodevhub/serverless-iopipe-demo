1a296eff0a8c2978b5ce5027b07e53748d6370a2
danilodevhub <danilo@cubo.network>
2018-03-28 11:10:04 -0300

    install io pipe plugin and impl three fn examples

--- handler.js
+++ handler.js
@@ -10,9 +10,46 @@ const hello = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
-module.exports = { hello };
+const withError = (event, context, callback) => {
+
+  const erro = variavelQueNaoExiste.atributo;
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+const withTrace = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Olá eu tenho trace =)'
+    }),
+  };
+
+  context.iopipe.mark.start('database');
+  setTimeout(() => {
+    // after database call is finished
+    context.iopipe.mark.end('database');
+
+    context.iopipe.mark.start('qqr-outra-coisa');
+    setTimeout(() => {
+      // after analytics call is finished
+      context.iopipe.mark.end('qqr-outra-coisa');
+
+      callback(null, response);
+    }, 1000);
+
+  }, 2000);
+  
+};
+
+module.exports = { hello, withError, withTrace };
